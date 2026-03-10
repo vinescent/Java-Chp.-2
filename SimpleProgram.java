fefe0e21217acf5933e719485a0731a876a3af15
vinescent <jshaurya21@gmail.com>
2026-03-09 20:49:08 -0400

    Add files via upload

---
+++ SimpleProgram.java
@@ -0,0 +1,25 @@
+// Shaurya Jain 
+// SimpleProgram
+// Calculates 5+6
+
+public class SimpleProgram { 
+  public static void main (String [ ] args) { 
+    
+    //variables 
+    int x;
+    int y; 
+    int z;
+	
+    //assignments 
+    x = 5; 
+    y = 6;
+	
+    //calculations 
+    z = x+ y;
+	
+    //output
+	System.out.println("X= " + x); 
+    System.out.println("Y= " + y); 
+    System.out.println("Z= " + z);
+  }
+}
