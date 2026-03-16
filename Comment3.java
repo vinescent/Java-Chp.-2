f153af738630be127d886fa79a69f0ffdac55ee5
vinescent <jshaurya21@gmail.com>
2026-03-16 10:22:10 -0400

    Add Comment3 program demonstrating payroll calculation and comment types

---
+++ Comment3.java
@@ -0,0 +1,37 @@
+// Shaurya Jain
+// Comment3
+/* This program demonstrates different types of comments.
+Single line comments start with a double slash.
+Multi-line comments start with a foward slash asterisk and end with
+an asterisk foward slash.
+Documentation comments start with a foward slash and 2 asterisks
+and end with a single asterisk foward slash. */
+
+/**
+This class creates a program that calculates company payroll. 
+*/
+
+public class Comment3 {
+  
+  /** 
+  The main method is the program's starting point.
+  */
+
+  public static void main(String[] args) {
+    
+    double payRate; 	// Holds the hourly pay rate
+	double hours; 		// Hours holds the hours worked
+    int employeeNumber; // Holds the employee number
+	
+    payRate = 15.00; 	// Initializes hourly pay rate
+    hours = 40.00; 		// Initializes hours worked
+	
+    double grossPay = payRate * hours; // declares and calculates gross pay
+	
+    System.out.println("Hourly pay:\t\t$" + payRate + "0");
+	System.out.println("Hours worked is:\t" + hours);
+    System.out.println("Gross pay is:\t\t$" + grossPay + "0");
+  }
+}
+// Run your program, then Copy/Paste your console output to a multi-line comment.
+/* Console Output: */
