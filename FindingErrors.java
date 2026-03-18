fb64319da6326c225bcc17733b0106d5098eec68
vinescent <jshaurya21@gmail.com>
2026-03-18 10:30:41 -0400

    Add FindingErrors program to calculate total price based on item price and quantity

---
+++ FindingErrors.java
@@ -0,0 +1,37 @@
+//Shaurya Jain
+//FindingErrors
+//This program calculates the total price of an order based on the price and quantity of items.
+
+public class FindingErrors {
+    public static void main(String[]args) {
+        
+        // variables
+        double price;
+        int quantity;
+        double total;
+        
+        // assignments
+        price = 5.25;
+        quantity = 8;
+        
+        // calculations
+        total = price * quantity;
+        
+        // output
+        System.out.println("Shaurya Jain");
+        System.out.println("FindingErrors");
+        System.out.println("This program calculates the total price of an order based on the price and quantity of items.");
+        System.out.println("quantity = " + quantity);
+        System.out.println("price = " + price);
+        System.out.println("total = " + total);
+    }
+}
+
+/* Output: 
+Shaurya Jain
+FindingErrors
+This program calculates the total price of an order based on the price and quantity of items.
+quantity = 8
+price = 5.25
+total = 42.0
+*/
