a063b9dface4f5f5779d7fa24042f57f2be1340b
vinescent <jshaurya21@gmail.com>
2026-03-18 10:43:23 -0400

    Add FunnyFace program to demonstrate printing a funny face using text characters

---
+++ FunnyFace.java
@@ -0,0 +1,31 @@
+// Shaurya Jain
+// FunnyFace
+// This program demonstrates how to print a funny face using text characters.
+
+public class FunnyFace {
+  public static void main(String[] args) {
+  
+  // Hair:
+    System.out.println("   ||||||||||   ");
+  //System.out.println("   |^^^^^^^^|   ");
+    
+  // Eyes:
+    System.out.println("     o   o      ");
+  //System.out.println("     0   0      ");
+    
+  // Nose:
+    System.out.println("       <        ");
+  //System.out.println("       >        ");
+    
+  // Mouth:
+    System.out.println("   ----------   ");
+  //System.out.println("   |________|   ");
+
+  }
+}
+/* Console Output:
+   ||||||||||   
+     o   o      
+       <     
+    ---------   
+*/
