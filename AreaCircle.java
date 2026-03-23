548335ace7ae27efa855e88d8b111ff2f2ecdb1f
vinescent <jshaurya21@gmail.com>
2026-03-23 10:17:48 -0400

    Add AreaCircle program to calculate the area of a circle based on the radius

---
+++ AreaCircle.java
@@ -0,0 +1,26 @@
+// Shaurya Jain
+// AreaCircle
+// This program calculates the area of a circle based on the radius provided.
+
+public class AreaCircle {
+  public static void main(String[] args) {
+    //variables
+    double pi; double r; double area;
+
+    //assignments 
+    pi = 3.1416; 
+    r = 2.5;
+
+    //calculations
+    area = pi * r * r;
+
+    //output 
+    System.out.println("r = " + r);
+    System.out.println("Area = " + area);
+  }
+}
+
+/* Console Output:
+r = 2.5
+Area = 19.635
+*/
